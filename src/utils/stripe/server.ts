f26d9a85287adc907cc335098b4213dccf8c484d
Mirko Dominguez <mirkodgz@Mac.Home>
2026-03-16 14:30:51 +0100

    fix(vercel): provide fallback for stripe api key during build

--- src/utils/stripe/server.ts
+++ src/utils/stripe/server.ts
@@ -1,6 +1,8 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY || 'sk_test_dummy_key_for_vercel_build', 
+  {
   // @ts-ignore
   apiVersion: '2023-10-16', // Use the latest stable version or your preferred version
   appInfo: {
